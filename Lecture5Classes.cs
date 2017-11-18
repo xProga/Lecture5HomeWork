bee3da4a443b1c882d97f87c32907b18623d6878
Vadim Proga Krylov <progavs@gmail.com>
2017-11-18 17:02:06 +0500

    first commit

---
+++ Lecture5Classes.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lecture5HomeWork
+{
+
+    /// <summary>
+    /// 1. Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 2-х до 10-ти символов, содержащая только буквы или цифры, и при этом цифра не может быть первой.
+    /// а) без использования регулярных выражений;
+    /// б) ** с использованием регулярных выражений.
+    /// </summary>
+    public static class Task1CheckLogPass
+    {
+        public static bool CheckLogPass(string login)
+        {
+            bool flag = false;
+            char[] log = login.ToCharArray();
+
+            for (int i = 0; i < log.Length; i++)
+            {
+                if (!(login.Length > 2 && ((char.IsNumber(log[i])) || (char.IsLetter(log[i])))))
+                {
+                    flag = false;
+                    break;
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+
+
+        public static bool CheckLogPassWithReg(string login)
+        {
+            bool flag = false;
+            var rule = @"\b([A-Za-z][A-Za-z0-9]{2,9})\b";
+            flag = Regex.IsMatch(login, rule);
+            return flag;
+        }
+    }
+
+    /// <summary>
+    /// 2. Разработать методы для решения следующих задач. Дано сообщение:
+    ///а) Вывести только те слова сообщения, которые содержат не более чем n букв;
+    ///б) Удалить из сообщения все слова, которые заканчиваются на заданный символ;
+    ///в) Найти самое длинное слово сообщения;
+    ///г) Найти все самые длинные слова сообщения.
+    ///Постараться разработать класс MyString.
+    /// </summary>
+    public class Task2WorkWithWords
+    {
+        private string CurrentString { get; set; }
+
+        public Task2WorkWithWords(string inputString)
+        {
+            CurrentString = inputString;
+        }
+
+        public string OutputWordsWithLengthLessThenNeed(int wordLength)
+        {
+            string str = String.Empty;
+            string[] strArray = CurrentString.Split(' ');
+            string rule = @"\b[A-Za-z]{1," + wordLength + "}\b";
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (Regex.IsMatch(strArray[i], rule))
+                {
+                    str += strArray[i] + " ";
+                }
+            }
+            return str;
+        }
+
+        public string DelWorldWitchEndsWithNeedSymbol(char symbol)
+        {
+            string str = String.Empty;
+            string[] strArray = CurrentString.Split(' ');
+            string rule = @"\b[A-Za-z]+[" + symbol.ToString() + "]\b";
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (!Regex.IsMatch(strArray[i], rule))
+                {
+                    str += strArray[i] + " ";
+                }
+            }
+            return str;
+        }
+
+        public string FingLongestWord()
+        {
+            string str = String.Empty;
+            string[] strArray = CurrentString.Split(' ');
+            int maxLength = 0;
+            string maxLengthWord = String.Empty;
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (strArray[i].Length > maxLength)
+                {
+                    maxLength = strArray[i].Length;
+                    maxLengthWord = strArray[i];
+                }
+            }
+
+            str = "Самое длинное слово: " + maxLengthWord + " с количеством символов в нем: " + maxLength;
+            return str;
+        }
+    }
+
+    /// <summary>
+    /// 3. *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой. Регистр можно не учитывать.
+    ///а) с использованием методов C#
+    ///б) * разработав собственный алгоритм
+    ///Например:
+    ///badc являются перестановкой abcd
+    /// </summary>
+
+    public class Task3CombinatoricalMethods
+    {
+        private string FirstString { get; set; }
+        private string SecondString { get; set; }
+
+        public Task3CombinatoricalMethods(string firstString, string secondString)
+        {
+            FirstString = firstString;
+            SecondString = secondString;
+        }
+
+        public bool FindCombination()
+        {
+            bool flag = false;
+
+            for (int i = 0; i < FirstString.Length; i++)
+            {
+                char[] charStr = FirstString.ToCharArray();
+                char selectedChar = charStr[i];
+                for (int j = 0; j < FirstString.Length - 1; j++)
+                {
+                    char tempChar;
+                    tempChar = charStr[j+1];
+                    charStr[j] = charStr[j + 1];
+                    charStr[j + 1] = tempChar;
+
+                    StringBuilder buildStr = new StringBuilder();
+                    string outputStr = buildStr.Append(charStr).ToString();
+                    if (outputStr.Equals(SecondString))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                { break; }
+            }
+            return flag;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lecture5HomeWork
+{
+    public static class Task1UserInterface
+    {
+        public static void TaskN1()
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-2]";
+            Console.Write("Введите логин для проверки:");
+            string login = Console.ReadLine();
+
+            Console.WriteLine("Каким алгоритмом вы бы хотели проверить логин?");
+            Console.WriteLine("\t1. Алгоритмом С#.");
+            Console.WriteLine("\t2. Регулярными выражениями.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        if (Task1CheckLogPass.CheckLogPass(login))
+                        {
+                            Console.WriteLine("Логин корректен");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case '2':
+                        if (Task1CheckLogPass.CheckLogPassWithReg(login))
+                        {
+                            Console.WriteLine("Логин корректен");
+                            Console.ReadKey();
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    public static class Task2UserInterface
+    {
+
+        public static void TaskN2()
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-3]";
+            Console.Write("Введите сообщение:");
+            string str = Console.ReadLine();
+            Task2WorkWithWords t2 = new Task2WorkWithWords(str);
+
+            Console.WriteLine("Что бы вы хотели сделать с сообщением?");
+            Console.WriteLine("\t1. Найти все слова, символов в кототм меньше чем задает пользователь.");
+            Console.WriteLine("\t2. Удалить все слова которые заканчиваются на поеределенный символ.");
+            Console.WriteLine("\t2. Найти самое длинное слово.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        Console.Write("Введите необходимое количество символов в слове:");
+                        t2.OutputWordsWithLengthLessThenNeed(int.Parse(Console.ReadLine()));
+                        break;
+                    case '2':
+                        Console.Write("Введите символ:");
+                        t2.DelWorldWitchEndsWithNeedSymbol(Convert.ToChar(Console.ReadLine()));
+                        break;
+                    case '3':
+                        Console.Write("Самое большое количество символов в слове: " + t2.FingLongestWord());
+                        break;
+                }
+            }
+        }
+    }
+
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-3]";
+
+            Console.WriteLine("Добрый день, уважаемый пользователь. Демонстрацию работы какой программы вы бы хотели увидеть?");
+            Console.WriteLine("\t1. Программа проверки логина на корректность.");
+            Console.WriteLine("\t2. Программа работы с введенным сообщением.");
+            Console.WriteLine("\t3. Программа определения, является ли одна строка перебором другой.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        Task1UserInterface.TaskN1();
+                        break;
+                    case '2':
+                        Task2UserInterface.TaskN2();
+                        break;
+                    case '3':
+                        Task3CombinatoricalMethods t3 = new Task3CombinatoricalMethods("abcd", "bcda");
+                        break;
+                }
+            }
+        }
+    }
+}
